4729f93ea62cd38fa66dccb2b81c6028373e97fc
Dmitriy Filatchev <o6jlako@mail.ru>
2024-12-09 09:30:52 +0300

    fix: zip files

--- release.config.js
+++ release.config.js
@@ -5,6 +5,14 @@ export default {
     '@semantic-release/commit-analyzer',
     '@semantic-release/npm',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          { path: 'build.zip', label: 'Build' },
+          { path: 'coverage.zip', label: 'Coverage' },
+        ],
+      },
+    ],
   ],
 };
